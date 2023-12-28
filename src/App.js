38c5608334dc78d6ed701762435fb3c5a88270a6
malerie-earle <malerie.augot@gmail.com>
2023-12-28 18:12:00 -0330

    more attempts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home.jsx";
 import Nav from "./components/Nav.jsx";
-import Projects from "Portfolio/src/pages/Projects.jsx";
+import Projects from "./pages/Projects.jsx";
 import PowerPoints from './pages/PowerPoints.jsx';
 import About from './pages/About.jsx';
 import Services from './pages/Services.jsx';
@@ -10,7 +10,7 @@ import DigitalMedia from "./pages/DigitalMedia.jsx";
 import Biography from "./pages/Biography.jsx";
 import Resume from "./pages/Resume.jsx"; 
 import Contact from "./pages/Contact.jsx";
-import Applications from "Portfolio/src/pages/Applications.jsx";
+import Applications from "./pages/Applications.jsx";
 import Websites from "./pages/Websites.jsx";
 
 function App() {
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,71 +1,71 @@
-import React from 'react';
-import { useRef } from 'react';
-import "../styles/index.css";
-import bioPic from "../images/20201024_130748.png";
+// import React from 'react';
+// import { useRef } from 'react';
+// import "../styles/index.css";
+// import bioPic from "../images/20201024_130748.png";
 
-const Contact = () => {
-  const form = useRef();
+// const Contact = () => {
+//   const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_TEMPLATE_ID,
-        form.current,
-        process.env.REACT_APP_PUBLIC_KEY
-      )
-      .then(
-        (result) => {
-          alert('message sent successfully...');
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+//     emailjs
+//       .sendForm(
+//         process.env.REACT_APP_SERVICE_ID,
+//         process.env.REACT_APP_TEMPLATE_ID,
+//         form.current,
+//         process.env.REACT_APP_PUBLIC_KEY
+//       )
+//       .then(
+//         (result) => {
+//           alert('message sent successfully...');
+//           console.log(result.text);
+//         },
+//         (error) => {
+//           console.log(error.text);
+//         }
+//       );
+//   };
 
-  return (
-    <div className="contact">
-      <h1>Contact Me</h1>
-      <div className = "contactGrid">
+//   return (
+//     <div className="contact">
+//       <h1>Contact Me</h1>
+//       <div className = "contactGrid">
 
       
-      <div className = "asideContact">
-        <h2>Get In Touch:</h2>
-        <h3>Malerie Earle</h3><br />
-        <p>Feel free to reach out! I'm always open to new opportunities and projects.</p>
-        <p>malerie.earle@gmail.com</p>
-        <ul>
-          <li>
-            <a href="http://www.linkedin.com/in/malerieearle0821" target="_blank">LinkedIn Profile</a>
-          </li>
-          <li>
-            <a href="https://github.com/malerie-earle" target="_blank">GitHub Profile</a>
-          </li>
-          <li>
-          <a href="#" target="_blank">Portfolio Website</a>
-          </li>
-        </ul>
-      </div>
-    <div className = "ContactForm">
+//       <div className = "asideContact">
+//         <h2>Get In Touch:</h2>
+//         <h3>Malerie Earle</h3><br />
+//         <p>Feel free to reach out! I'm always open to new opportunities and projects.</p>
+//         <p>malerie.earle@gmail.com</p>
+//         <ul>
+//           <li>
+//             <a href="http://www.linkedin.com/in/malerieearle0821" target="_blank">LinkedIn Profile</a>
+//           </li>
+//           <li>
+//             <a href="https://github.com/malerie-earle" target="_blank">GitHub Profile</a>
+//           </li>
+//           <li>
+//           <a href="#" target="_blank">Portfolio Website</a>
+//           </li>
+//         </ul>
+//       </div>
+//     <div className = "ContactForm">
       
-      <form className='cf'>
-        <h2>Send a Message</h2>
-          <input type='text' placeholder='Name' name='user_name' />
-          <input type='email' placeholder='Email address' name='user_email' />
+//       <form className='cf'>
+//         <h2>Send a Message</h2>
+//           <input type='text' placeholder='Name' name='user_name' />
+//           <input type='email' placeholder='Email address' name='user_email' />
 
-          <textarea name='message' type='text' placeholder='Message'></textarea>
+//           <textarea name='message' type='text' placeholder='Message'></textarea>
       
-        <input type='submit' value='Submit' id='input-submit' />
-      </form>
-    </div>
-    <div className = "rightSide">
-      <img src = {bioPic} alt = "Bio Pic" className = "bioPicContact" />
-    </div>
-    </div>
-    </div>
-  );
-}
-export default Contact;
+//         <input type='submit' value='Submit' id='input-submit' />
+//       </form>
+//     </div>
+//     <div className = "rightSide">
+//       <img src = {bioPic} alt = "Bio Pic" className = "bioPicContact" />
+//     </div>
+//     </div>
+//     </div>
+//   );
+// }
+// export default Contact;
